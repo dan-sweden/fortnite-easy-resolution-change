304cfe286ff66deb0ce852d9839762bc6a0d4ab4
Dan Johansson <dan.sweden@gmail.com>
2025-01-04 18:31:05 +0100

    Update fortnite-easy-resolution-change-startup.cmd

--- fortnite-easy-resolution-change-startup.cmd
+++ fortnite-easy-resolution-change-startup.cmd
@@ -4,11 +4,7 @@
 @REM Using the Startup Folder:
 @REM 1/ Open the Run dialog box: Press Windows key + R.
 @REM 2/ Open the Startup folder: Type shell:startup and press Enter.
-@REM 3/ Create a new text file: Right-click in the folder, select New, then Text Document.
-@REM 4/ Edit the text file: Open the text file in Notepad and paste the following command, replacing the path with your script's path and adding your argument:
-@REM powershell "C:\path\to\powershell\script.ps1" -Argument "YourArgument"
-@REM 5/ Save and rename the file: Save the file, close Notepad, and rename the file to fortnite-easy-resolution-change-startup.cmd
-@REM 6/ Ensure file extensions are visible: If you don't see the .cmd extension, make sure your file explorer settings show file extensions
+@REM 3/ Copy this file to the Startup folder.
 
 
 @REM This is the main part of the script. It will run the PowerShell script with the argument.
